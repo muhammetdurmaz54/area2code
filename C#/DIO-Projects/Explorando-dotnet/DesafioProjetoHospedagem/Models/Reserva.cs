4921cb6b3540cbd81680d09bbff59bfa6f0398fe
DevisonSantana <devisonsantana@hotmail.com>
2023-10-20 03:42:01 -0300

    add <Explorando dotnet> Desafio de hospedagem || remove <obj, bin> Fundamentos-dotnet

---
+++ C#/DIO-Projects/Explorando-dotnet/DesafioProjetoHospedagem/Models/Reserva.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception ("A quantidade de hóspedes foi excedida!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.90M;
+            }
+
+            return valor;
+        }
+    }
+}
